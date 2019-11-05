687fe6d4808d31c997d5f9350fe66a8510923414
eclipse_user <eclipse_user@hotmail.com>
2019-11-05 23:11:13 +0200

    test again about the .gitignore file.

--- src/main/java/com/huijinyuan/git/Happy.java
+++ src/main/java/com/huijinyuan/git/Happy.java
@@ -3,6 +3,7 @@ package com.huijinyuan.git;
 public class Happy {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("Testing...");
 		System.out.println("Testing...again about the .gitignore.");
 		
